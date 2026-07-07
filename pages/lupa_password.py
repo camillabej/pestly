fb826f4246c3f6c6907fe3c9453e49a563c49827
Camilla <camillaasmatt@gmail.com>
2026-07-07 13:43:28 +0700

    Menambahkan fitur lupa password

--- pages/lupa_password.py
+++ pages/lupa_password.py
@@ -6,22 +6,15 @@ st.set_page_config(
     page_icon="🔑",
     layout="centered"
 )
-#proteksi halaman
-if not st.session_state.get("logged_in"):
-    st.switch_page("pages/lupa_password.py")
-    st.stop()
-
-def load_css():
-    with open("styles/home.css", encoding="utf-8") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-load_css()
 
 st.markdown("""
 <style>
-[data-testid="stSidebarNav"] { display: none; }
+[data-testid="stSidebarNav"] {
+    display: none;
+}
 </style>
 """, unsafe_allow_html=True)
+
 st.title("🔑 Lupa Password")
 
 
